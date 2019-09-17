bf6e81d4f5c209ccb2385769b40c690d882fecdf
Ernazar <ernazar.nks@gmail.com>
2019-09-17 16:50:06 +0500

    Initial commit

---
+++ app/src/main/java/uz/aliensoft/uzpos/Product.kt
@@ -0,0 +1,3 @@
+package uz.aliensoft.uzpos
+
+data class Product(val number: Int, val name: String, val code: String, val price: String, val amount: Int, val sum:  Double)
---
+++ app/src/main/java/uz/aliensoft/uzpos/itemViewHolder.kt
@@ -0,0 +1,28 @@
+package uz.aliensoft.uzpos
+
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import uz.aliensoft.uzpos.R
+
+class ProductItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val productNumber: TextView = itemView.findViewById(R.id.product_number_id)
+    private val productName: TextView = itemView.findViewById(R.id.product_name_id)
+    private val productCode: TextView = itemView.findViewById(R.id.product_code_id)
+    private val productPrice: TextView = itemView.findViewById(R.id.product_price_id)
+    private val productAmountMinus: Button = itemView.findViewById(R.id.product_amount_minus_id)
+    private val productAmountText: TextView = itemView.findViewById(R.id.product_amount_text_id)
+    private val productAmountPlus: Button = itemView.findViewById(R.id.product_amount_text_id)
+    private val productSum: TextView = itemView.findViewById(R.id.product_sum_id)
+    private val productClear: Button = itemView.findViewById(R.id.product_clearBtn_id)
+    fun populateProduct(product: Product) {
+        productNumber.text = product.number.toString()
+        productName.text = product.name
+        productCode.text = product.code
+        productPrice.text = product.price
+        productAmountText.text = product.amount.toString()
+        productSum.text = product.sum.toString()
+
+    }
+}
